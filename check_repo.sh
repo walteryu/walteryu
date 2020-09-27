cf9d5b286c62968bf4a5cd9e767db5f4b12cbc25
wyu <kharma@gmail.com>
2020-09-27 00:39:15 -0700

    add script

---
+++ check_repo.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "*** grep -nrw API ***"
+grep -nrw 'API'
+echo ""
+
+echo "*** grep -nrw KEY ***"
+grep -nrw 'KEY'
+echo ""
+
+echo "*** grep -nrw HASH ***"
+grep -nrw 'HASH'
+echo ""
+
+echo "*** grep -nrw API_KEY ***"
+grep -nrw 'API_KEY'
+echo ""
+
+echo "*** grep -nrw PASSWORD ***"
+grep -nrw 'PASSWORD'
+echo ""
+
+echo "*** grep -nrw USERNAME ***"
+grep -nrw 'USERNAME'
+echo ""
+
+echo "*** grep -nrw CREDENTIALS ***"
+grep -nrw 'CREDENTIALS'
+echo ""
+
+echo "*** grep -nrw SECRET ***"
+grep -nrw 'SECRET'
+echo ""
+
+echo "*** grep -nrw api ***"
+grep -nrw 'api'
+echo ""
+
+echo "*** grep -nrw key ***"
+grep -nrw 'key'
+echo ""
+
+echo "*** grep -nrw hash ***"
+grep -nrw 'hash'
+echo ""
+
+echo "*** grep -nrw api_key ***"
+grep -nrw 'api_key'
+echo ""
+
+echo "*** grep -nrw password ***"
+grep -nrw 'password'
+echo ""
+
+echo "*** grep -nrw username ***"
+grep -nrw 'username'
+echo ""
+
+echo "*** grep -nrw credentials ***"
+grep -nrw 'credentials'
+echo ""
+
+echo "*** grep -nrw secret ***"
+grep -nrw 'secret'
+echo ""
